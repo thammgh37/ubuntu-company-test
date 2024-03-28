c27aa51f51f62c95a7980a0c1e03e525ea5c8047
thammgh37 <hoanganh03052003@gmail.com>
2024-03-29 04:06:49 +0700

    seperate file

--- src/mystring.cpp
+++ src/mystring.cpp
@@ -1,43 +1,63 @@
-#include <iostream>
-#include <string>
-#include <cstring>
-using namespace std;
+#include "mystring.h"
 
-class MyString{
-    private:
-        char* str;
-        int size;
-    public:
-        MyString(char* str = NULL,int size = 0) : str{str},size{size}{}
-        MyString(const MyString& source){
-            char* buff = new char[size+1];
-            strcpy(buff,source.str);
-            delete [] this->str;
-            this->str = buff;
-            this->size = source.size;
-        }
-        MyString(MyString&& source){
-            this->size = source.size;
-            this->str = source.str;
-            source.str = NULL;
-            source.size = 0;
-        }
-        ~MyString(){
-            cout << "Delete :";
-            for (char* i = str; i < str+size ;i ++){
-                cout << *i << endl;
-            }
-            delete[] str;
-        }
-        MyString& operator=(const MyString& source){
-            if(this == & source){
-                return *this;
-            }
-            char* buff = new char[source.size+1];
-            strcpy(buff,source.str);
-            delete [] this->str;
-            this->str = buff;
-            this->size = source.size;
-            return *this;
-        }
-};
+MyString::MyString(char* str,size_t size) : str{str},size{strlen(str)}{}
+
+MyString::MyString(const MyString& source){
+    char* buff = new char[size+1];
+    strcpy(buff,source.str);
+    delete [] this->str;
+    this->str = buff;
+    this->size = source.size;
+}
+MyString::MyString(MyString&& source){
+    this->size = source.size;
+    this->str = source.str;
+    source.str = NULL;
+    source.size = 0;
+}
+MyString::~MyString(){
+    cout << "Delete :";
+    for (char* i = str; i < str+size ;i ++){
+        cout << *i;
+    }
+    cout<<"\n";
+    delete[] str;
+}
+
+MyString& MyString::operator=(const MyString& source){
+    if(this == & source){
+        return *this;
+    }
+    char* buff = new char[source.size+1];
+    strcpy(buff,source.str);
+    delete [] this->str;
+    this->str = buff;
+    this->size = source.size;
+    return *this;
+}
+MyString& MyString::operator=(MyString&& source){
+    if(this == & source){
+        return *this;
+    }
+    this->str = source.str;
+    this->size = source.size;
+    source.str = NULL;
+    source.size = 0;
+    return *this;
+}
+
+MyString& MyString::operator++(){
+    char* buff = new char[this->size+1];
+    for (int i = 0; i<size; i++){
+        buff[i] = toupper(this->str[i]);
+    }
+    delete[] this->str;
+    this->str = buff;
+    return *this;
+}
+void MyString::print(){
+    for(int i=0;i<size;i++){
+        cout << this->str[i];
+    }
+    cout << endl;
+}
